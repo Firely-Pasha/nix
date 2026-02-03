eb8ad07df34536ee919ac0d5b0a962c02ac7f002
pavel <tiddiebiter@gmail.com>
2026-02-04 00:26:36 +0300

    feat: update

--- users/common.linux.nix
+++ users/common.linux.nix
@@ -11,6 +11,7 @@
     unzip
   ];
   home.shellAliases = {
+    "home-switch" = "home-manager --flake ~/.nix#pavel@linux --extra-experimental-features 'nix-command flakes' switch";
     "hs" = "home-switch";
     "home-edit" = "cd ~/.nix && nvim . && cd - > /dev/null";
     "he" = "home-edit";
